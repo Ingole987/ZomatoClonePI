bb3f53ffebe674e817f384da2e77673596b5f64a
Ingole987 <pratik.ingole98@gmail.com>
2022-11-17 16:02:08 +0530

    Completed Server, added babel, added mongo,modules

---
+++ Server/database/allModules.js
@@ -0,0 +1,17 @@
+import {FoodModel} from '.Food'
+import {RestaurantModel} from '.Restaurant'
+import {MenuModel} from '.Menu'
+import {OrderModel} from '.Order'
+import {ImageModel} from '.Image'
+import {ReviewModel} from '.Review'
+import {UserModel} from '.User'
+
+export {
+FoodModel,
+RestaurantModel,
+MenuModel,
+OrderModel,
+ImageModel,
+ReviewModel,
+UserModel
+}
---
+++ Server/index.js
@@ -0,0 +1,34 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+// Database Connection
+
+import ConnectDB from "./database/connection"
+
+dotenv.config()
+
+const zomatoPI = express()
+
+zomatoPI.use(express.json())
+
+zomatoPI.get("/" , (req,res) => {
+    res.json({
+        message : "Server is running"
+    })
+})
+
+const PORT = 4000;
+
+zomatoPI.listen(PORT, () => {
+//    ConnectDB()
+//    .then(() => {
+//     console.log("Server is running !!")
+//    })
+
+//    .catch((error) => {
+//     console.log("Server is running, but DB connection failed")
+//     console.log(error)
+//    })
+
+   console.log("Server is running !!")
+}) 
